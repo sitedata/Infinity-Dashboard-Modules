f0dbc7531b2068d7308a784e0a8a2ad53e586858
Richard Guay <raguay@customct.com>
2019-01-07 20:33:30 +0700

    added better error messages for Local Interface module.

--- community/LocalInterface.infinitymodule/main.js
+++ community/LocalInterface.infinitymodule/main.js
@@ -11,7 +11,7 @@ var os = require('os');
 let interface = fiplab.arguments.interface;
 var ifaces = os.networkInterfaces();
 var address = ""
-
+var padd = ""
 Object.keys(ifaces).forEach(function(ifname) {
   ifaces[ifname].forEach(function (iface) {
     if ('IPv4' !== iface.family || iface.internal !== false) {
@@ -21,10 +21,23 @@ Object.keys(ifaces).forEach(function(ifname) {
 
     if(ifname === interface) {
       address = iface.address;
+    } else if(padd === "") {
+      padd = ifname;
     }
   });
 });
 let returnOptions = {
     'onclick': 'http://'+ address
 };
-fiplab.exit(address, true, returnOptions);
+
+if (address == "") {
+  let notification = {
+    id: 'localAddressMessage',
+    message: "The '" + interface + "' interface wasn't found. Maybe '" + padd +"'?"
+  };
+  fiplab.notify(notification);
+  fiplab.exit('Interface not found.', false);
+} else {
+  fiplab.exit(address, true, returnOptions);
+}
+
